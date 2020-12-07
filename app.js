d31692f79bff87dc28f86c132741a1c5f734c1b1
xmmti <it7mdX@gmail.com>
2020-12-07 15:01:56 +0300

    add project

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var study = new Vue({
   el: "#vue-app",
   data: {
     heading: "Vue.js",
-    text: "Happiness makes up in height what it lacks in length."
+    text: "Happiness makes up in height what it lackl."
   }
 });
